22b8b3156207e26787a0fddc757eee8a360c5bd7
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-02-22 15:54:21 +0100

    Przywodca ciagu

---
+++ 01Podstawy/02PrzywodcaCiagu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02PrzywodcaCiagu {
+    class Program {
+        static int PrzywodcaCiagu(int [] A) {
+            int ile = 0;
+            int j = 0;
+            int n = A.Length;
+
+            for (int i = 1; i <= n; i++)
+                if (ile == 0) {
+                    ile = ile + 1;
+                    j = i;
+                }
+                else if (A[i] == A[j])
+                    ile = ile + 1;
+                    else ile = ile - 1;
+
+            return A[j];
+        }
+
+            static void Main(string[] args) {
+        }
+    }
+}
