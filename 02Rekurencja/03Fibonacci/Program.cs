357ba34326e6fe1843252dc535f9febd07622346
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-14 17:34:08 +0100

    Fibonacci big&fast

--- 02Rekurencja/03Fibonacci/Program.cs
+++ 02Rekurencja/03Fibonacci/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 
 namespace _03Fibonacci {
     class Program {
@@ -12,8 +9,9 @@ namespace _03Fibonacci {
             else return fib_rek(n - 1) + fib_rek(n - 2);
         }
 
-        public static long fib_iter(long n) {
-            int i, w = 0, f0 = 0, f1 = 1;
+        public static BigInteger fib_iter(long n) {
+            int i;
+            BigInteger w = 0, f0 = 0, f1 = 1;
 
             for(i = 2; i < n + 1; i++) {
                 w = f0 + f1;
@@ -23,21 +21,48 @@ namespace _03Fibonacci {
             return w;
         }
 
-        static void Main(string[] args) {
-            int N_start = 20, N_stop = 35;
+        static BigInteger[,] multiply(BigInteger[,] a, BigInteger[,] b) {
+            BigInteger[,] result = new BigInteger[2, 2];
+            result[0, 0] = BigInteger.Add(BigInteger.Multiply(a[0, 0], b[0, 0]), BigInteger.Multiply(a[0, 1], b[1, 0]));
+            result[0, 1] = BigInteger.Add(BigInteger.Multiply(a[0, 0], b[0, 1]), BigInteger.Multiply(a[0, 1], b[1, 1]));
+            result[1, 0] = BigInteger.Add(BigInteger.Multiply(a[1, 0], b[0, 0]), BigInteger.Multiply(a[1, 1], b[1, 0]));
+            result[1, 1] = BigInteger.Add(BigInteger.Multiply(a[1, 0], b[0, 1]), BigInteger.Multiply(a[1, 1], b[1, 1]));
+            return result;
+        }
 
+        static BigInteger[,] fibMatrixPower(BigInteger[,] m, int n) {
+            if(n == 1) return m;
+            else if(n % 2 == 0) return fibMatrixPower(multiply(m, m), n / 2);
+            else return multiply(fibMatrixPower(multiply(m, m), (n - 1) / 2), m);
+        }
+
+        public static BigInteger fibobacciFast(int n) {
+            BigInteger[,] result = new BigInteger[2, 2] { { 1, 1 }, { 1, 0 } };
+            return fibMatrixPower(result, n)[0, 1];
+        }
+
+        static void Main(string[] args) {
+            int N_start = 1000, N_stop = 1010;
+            BigInteger res;
             Stopwatch sw = new Stopwatch();
             for(int i = N_start; i < N_stop; i++) {
+                //sw.Reset();
+                //sw.Start();
+                //Console.Write("rek({0:G3}) = {1:G7} ", i, fib_rek(i));
+                //sw.Stop();
+                //Console.Write("{0:F5} ", sw.Elapsed.TotalMilliseconds);
                 sw.Reset();
                 sw.Start();
-                Console.Write("Fib_rek({0}) = {1}", i, fib_rek(i));
+                res = fib_iter(i);
                 sw.Stop();
-                Console.WriteLine("\t {0}", sw.Elapsed.TotalMilliseconds);
+                Console.Write("itr({0:G3}) = {1:G7} ", i, res);
+                Console.WriteLine("{0:F5} ", sw.Elapsed.TotalMilliseconds);
                 sw.Reset();
                 sw.Start();
-                Console.Write("Fib_itr({0}) = {1}", i, fib_iter(i));
+                res = fibobacciFast(i);
                 sw.Stop();
-                Console.WriteLine("\t {0}", sw.Elapsed.TotalMilliseconds);
+                Console.Write("fast({0:G3}) = {1} ", i, res);
+                Console.WriteLine("{0:F5}", sw.Elapsed.TotalMilliseconds);
             }
         }
     }
