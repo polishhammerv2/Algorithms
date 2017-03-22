1d57e23cdc732aa34f0e64015c8c0fb80102fbfb
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-03-22 15:32:38 +0100

    Listy begin

---
+++ 02Rekurencja/06Listy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06Listy {
+    public class Node {
+        public int value;
+        public Node nextNode;
+
+        public Node(int v) {
+            this.value = v;
+            this.nextNode = null;
+        }
+    }
+
+    public class Lista {
+        public Node head;
+        public Node tail;
+        
+         
+    }
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
