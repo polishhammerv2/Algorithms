5e7b9480afcbf978ed3a249d9e40b2cf76fd8351
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2017-04-05 16:43:31 +0200

    Prawie plecak

---
+++ 04Zachlanne/02Plecak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Plecak {
+    class Program {
+        static void Main(string[] args) {
+            int pojemnoscPlecaka = 20;
+            int[] profity = { 25, 24, 15 };
+            int[] wagi = { 18, 15, 10 };
+
+            double[] gestoscProfitu = new double[profity.Length];
+
+            for(int i = 0; i < profity.Length; i++)
+                gestoscProfitu[i] = (1.0 * profity[i]) / wagi[i];
+
+            Array.Sort(gestoscProfitu);
+            Array.Reverse(gestoscProfitu);
+
+            int indeks = 0;
+            while(pojemnoscPlecaka > 0 && indeks < profity.Length - 1) {
+                // .... 
+            }
+        }
+    }
+}
