7b5d6b640fe9e7f472ce7df7fb7646e1f7f324bb
Magdalena <m.j.sibila@gmail.com>
2017-04-22 21:20:30 +0200

    Dodano funkcję sortującą. Można dodać element skracający działanie funkcji dla przywódcy znajdującego się w pierwszej połowie tablicy.

--- Studenci/2017_Sibila_Magdalena/Przywodca/Program.cs
+++ Studenci/2017_Sibila_Magdalena/Przywodca/Program.cs
@@ -10,9 +10,35 @@ namespace Przywodca
     {
         public static int Przywodca(int[] tab)
         {
+            Console.WriteLine("Start");
+            int ile = 0;
+            int j = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                
+                if (ile == 0)
+                {
+                    ile++;
+                    j = i;
+                }
+                else if (tab[i] == tab[j])
+                {
+                    ile++;
+                }
+                else ile--;
+
+                //Console.WriteLine("It:" + i+", ile:"+ile+", j:"+j+", tab[j]:"+tab[j]);
+            }
+            Console.WriteLine("Koniec");
+            return tab[j];
+        }
+
+        public static int PrzywodcaSort(int[] tab)
+        {
 
             int ile = 0;
             int j = 0;
+            Array.Sort(tab);
             for (int i = 0; i < tab.Length; i++)
             {
                 if (ile == 0)
@@ -22,16 +48,20 @@ namespace Przywodca
                 }
                 else if (tab[i] == tab[j])
                 {
-                    ile = ile++;
+                    ile++;
                 }
+             //   else if (ile > tab.Length/2)
+               //     break;
+
                 else ile--;
             }
-             return tab[j];
+            return tab[j];
         }
         static void Main(string[] args)
         {
-            int[] tab = { 1, 1, 2, 2, 1, 3, 1, 4, 1, 7, 1, 6, 2, 1};
-            Console.Write(Przywodca(tab));
+            int[] tab = { 1, 1, 2, 2, 1, 3, 1, 4, 1, 7, 1, 1, 2, 1, 6};
+            Console.WriteLine(Przywodca(tab));
+            Console.WriteLine(PrzywodcaSort(tab));
             Console.Read();
         }
     }
